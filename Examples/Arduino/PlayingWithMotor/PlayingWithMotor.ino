cfebf65fe50de38a5f6d29b0b9761c4911bf9c49
tfmontague <tfmontague@gmail.com>
2014-05-03 16:40:31 -0700

    Update PlayingWithMotor to run Turnigy SK3 brushless motor

--- Examples/Arduino/PlayingWithMotor/PlayingWithMotor.ino
+++ Examples/Arduino/PlayingWithMotor/PlayingWithMotor.ino
@@ -1,18 +1,28 @@
 #include <Servo.h>
  
+int sensor = A0;
+int motor = 9;
+
 Servo esc;
-int throttlePin = A0;
+int sensorValue = 0;
  
-void setup()
-{
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communi  cation at 9600 bits per second:
   Serial.begin(9600);
-  esc.attach(6);
+  pinMode(sensor, INPUT);
+  esc.attach(motor);
 }
- 
-void loop()
-{
-  int throttle = analogRead(throttlePin);
-  throttle = map(throttle, 0, 1023, 0, 179);
-  Serial.println(throttle);
-  esc.write(throttle);
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  sensorValue = analogRead(sensor);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  sensorValue = map(sensorValue, 0, 1023, 1000, 2000);
+  // at 1032 it's still a stop
+  // sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  esc.writeMicroseconds(sensorValue);
 }
